406688dfd1edff11d99b995c2efdfe79e8f68ac9
Alexandre Araújo <a1lexandre.na@gmail.com>
2021-01-25 12:11:41 -0300

    Creating initial activities

---
+++ app/src/main/java/com/android/desafiojogos4/view/UserRegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.android.desafiojogos4.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.desafiojogos4.R
+
+class UserRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_registration)
+    }
+}
